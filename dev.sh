b457c92ed4417c339b1579895540a43f61b55f22
dev.estudos <dev.estudos15@outlook.com>
2022-06-12 00:57:53 -0300

    docker-compose

--- dev.sh
+++ dev.sh
@@ -83,7 +83,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo echo  -S | add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
 sudo apt install -y docker-ce
-sudo systemctl status docker
+sudo apt install -y docker-compose
 
 ########################### atualizacao e limpeza ###################################
 
