f7653dd09072a501b23fcf61800e7a7f0d436919
dev.estudos <dev.estudos15@outlook.com>
2022-06-16 00:07:48 -0300

    shutdown

--- update.sh
+++ update.sh
@@ -1,5 +1,19 @@
+Purple='\033[0;35m'
+NC='\033[0m' # No Color
+
+printf "${Purple}*************************************************************${NC} \n"
+echo "   desligar ao encerrar o processo?"
+echo "   digite: sim para desligar ao termino do processo"
+echo "   digite: não para nãp desligar ao termino do processo"
+printf "${Purple}*************************************************************${NC} \n"
+read varname
 
  sudo  apt-get update
  sudo  apt-get upgrade -y
  sudo  apt-get autoremove
  sudo  apt-get autoclean
+
+if [ "$varname" == "sim" ]; then
+shutdown
+fi    
+
