83e05229eac8509594c3bf769733d8243913db0e
dev.estudos <dev.estudos15@outlook.com>
2022-06-18 14:21:07 -0300

    portainer

--- dev.sh
+++ dev.sh
@@ -106,6 +106,15 @@ sudo echo  -S | add-apt-repository "deb [arch=amd64] https://download.docker.com
 sudo apt update
 sudo apt install -y docker-ce
 sudo apt install -y docker-compose
+###########################  portainer ###################################
+
+docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:2.9.3
+########################### portainer ###################################
 
 ########################### atualizacao e limpeza ###################################
 
