d9ab80ff2711882c31d177321d26009442d5f6de
jose.games15 <76719190+joseEstudos@users.noreply.github.com>
2022-07-06 22:45:57 -0300

    Update gitCOnfig.sh

--- gitCOnfig.sh
+++ gitCOnfig.sh
@@ -1,4 +1,4 @@
 #não rodor em modo root/sudo
-git config --global diff.external /home/diff.py
+git config --global diff.external  '/home/diff.py'
 git config --global merge.tool meld
-git config --global pull.rebase false'
+git config --global pull.rebase false
